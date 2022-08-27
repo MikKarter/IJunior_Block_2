3e0dc68bfe05fc4d7bfa6536e3d52da44b0d5f29
MikKarter <66348029+MikKarter@users.noreply.github.com>
2022-08-27 20:53:59 +0900

    Add files via upload

---
+++ 2.Task_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace _2.Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int phraseRepeat;
+            int repeatCounter;
+            string userPhrase;
+            Console.WriteLine("Hello!");
+            Console.WriteLine("What phrase will be write on screen?");
+            userPhrase = Console.ReadLine();
+            Console.WriteLine("How much time?");
+            phraseRepeat = Convert.ToInt32(Console.ReadLine());
+            for (repeatCounter = 0; repeatCounter < phraseRepeat; repeatCounter++ )
+                {
+                Console.WriteLine(userPhrase);
+                }
+        }
+    }
+}
