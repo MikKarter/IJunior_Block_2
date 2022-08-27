a1a1d589c89abb1163e1be118dc9457c5cad0e5b
MikKarter <66348029+MikKarter@users.noreply.github.com>
2022-08-27 21:30:02 +0900

    Add files via upload

---
+++ 2.Task_2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _2.Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Добро пожаловать в какое то место");
+            Console.WriteLine("Будем пытаться выйти из него по кодовому слову");
+            string codePhrase = "Exit";
+            string userPhrase;
+            bool starsWaitingCodePhrase = true;
+            while (starsWaitingCodePhrase = true)
+            {
+                Console.Write("Чтобы закончить с этим введите кодовое слово:");
+                userPhrase = Console.ReadLine();
+                if (userPhrase == codePhrase)
+                {
+                    Console.WriteLine("Моё почтение, вы угадали. До свидания.");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Увы, не-а. Давайте попробуем ещё раз.");
+                    Console.WriteLine("Попробуйте снова ввести кодовое слово:");
+                }
+            }    
+        }
+    }
+}
