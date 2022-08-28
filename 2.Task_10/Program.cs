721de31f46773141342f552bb6c5c15009807218
MikKarter <66348029+MikKarter@users.noreply.github.com>
2022-08-29 08:58:15 +0900

    Add files via upload

---
+++ 2.Task_10/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Task_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random randomNumber = new Random();
+            int mainRandomNumber = randomNumber.Next(0, 100);
+            int theDegree = 0;
+            int numberTwo = 2;
+            int numberTwoInDegree= 1;
+
+            while (numberTwoInDegree < mainRandomNumber)
+            {
+                numberTwoInDegree = numberTwoInDegree * numberTwo;
+                theDegree++;
+            }
+
+            Console.WriteLine("Random number is " + mainRandomNumber);
+            Console.WriteLine("The minimum number (" +numberTwo + "^N) that is greater than " + mainRandomNumber + " that " + numberTwoInDegree);
+            Console.WriteLine("To get this number, you need to raise " +numberTwo +" to the degree " + theDegree);
+        }
+    }
+}
