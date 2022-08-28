2ace29a5d0a64803e451905e75875b74627000ac
MikKarter <66348029+MikKarter@users.noreply.github.com>
2022-08-28 16:24:29 +0900

    Update Program.cs

--- 2.Task_2/Program.cs
+++ 2.Task_2/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace _2.Task_2
 {
@@ -10,22 +10,25 @@ namespace _2.Task_2
             Console.WriteLine("Будем пытаться выйти из него по кодовому слову");
             string codePhrase = "Exit";
             string userPhrase;
-            bool starsWaitingCodePhrase = true;
-            while (starsWaitingCodePhrase = true)
+            bool waitingCodePhrase = true;
+
+            while (waitingCodePhrase == true)
             {
                 Console.Write("Чтобы закончить с этим введите кодовое слово:");
                 userPhrase = Console.ReadLine();
+
                 if (userPhrase == codePhrase)
                 {
                     Console.WriteLine("Моё почтение, вы угадали. До свидания.");
-                    break;
+                    waitingCodePhrase = false;
                 }
+
                 else
                 {
                     Console.WriteLine("Увы, не-а. Давайте попробуем ещё раз.");
                     Console.WriteLine("Попробуйте снова ввести кодовое слово:");
                 }
-            }    
+            }
         }
     }
 }
