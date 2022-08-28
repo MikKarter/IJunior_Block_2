9a0a9c1b7af7ead19aa3707f5e2f2d5b2c575345
MikKarter <66348029+MikKarter@users.noreply.github.com>
2022-08-28 19:34:10 +0900

    Add files via upload

---
+++ 2.Task_7/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Task_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName;
+            string pictureSybmol;
+            int lengthSymbolRow;
+
+            Console.WriteLine("Whats your name?");
+            userName = Console.ReadLine();
+            Console.WriteLine("Please, input liked symbol");
+            pictureSybmol = Console.ReadLine();
+            lengthSymbolRow = userName.Length + 2;
+
+            for (int i = 0; i < lengthSymbolRow; i++)
+            {
+                Console.Write(pictureSybmol);
+            }
+
+            Console.WriteLine("");
+            Console.Write(pictureSybmol + userName + pictureSybmol + "\n");
+            
+            for (int i = 0; i < lengthSymbolRow; i++)
+            {
+                Console.Write(pictureSybmol);
+            }
+
+            Console.WriteLine("\n");
+        }
+    }
+}
