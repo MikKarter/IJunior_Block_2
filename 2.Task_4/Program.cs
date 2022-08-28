afb20f29ed5ba77ed85145a8562adbedbe3ff195
MikKarter <66348029+MikKarter@users.noreply.github.com>
2022-08-28 16:33:03 +0900

    Update Program.cs

--- 2.Task_4/Program.cs
+++ 2.Task_4/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,17 +10,19 @@ namespace _2.Task_4
     {
         static void Main(string[] args)
         {
-            Random rand = new Random();
-            int number = rand.Next(-5, 101);
-            int summNumbers = 0, divisor3 = 3, divisor5 = 5; 
+            Random randomNumber = new Random();
+            int number = randomNumber.Next(-100, 101);
+            int summNumbers = 0;
+            int divisorOne = 3;
+            int divisorTwo = 5; 
             Console.WriteLine(number);
 
             for (int currentNumber = 0; currentNumber <= number; currentNumber++)
             {
-                if (currentNumber % divisor3 == 0 | currentNumber % divisor5 == 0)
+                if (currentNumber % divisorOne == 0 | currentNumber % divisorTwo == 0)
                 {
                     Console.WriteLine(currentNumber + "  является кратным 3 или 5");
-                    summNumbers = summNumbers + currentNumber;
+                    summNumbers += currentNumber;
                 }
             }
             Console.WriteLine("Сумма всех положительных чисел меньше " + number + ", кратных 3 или 5 равна " + summNumbers);
