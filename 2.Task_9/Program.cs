a34825f8693de274b806084bee1be5abda51ffc8
MikKarter <66348029+MikKarter@users.noreply.github.com>
2022-08-29 08:43:43 +0900

    Add files via upload

---
+++ 2.Task_9/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Task_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Random randomForNumberN= new Random();
+           
+            int numberN = randomForNumberN.Next(1,28);
+            int counterNumbersMultiplesN = 0;
+            int summNumberN = 0;
+
+            Console.WriteLine(numberN);
+
+            for (int i = 100;100<=i&i<=999; i++)
+            {
+                while (summNumberN<100)
+                {
+                    summNumberN += numberN;
+                }
+                if (summNumberN >= 100 & summNumberN < 1000)
+                {
+                    counterNumbersMultiplesN++;
+                    summNumberN += numberN;
+                }
+            }
+            Console.WriteLine(counterNumbersMultiplesN);
+        }
+    }
+}
