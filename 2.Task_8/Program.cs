2fde1c6196e4b80891dcf4b1530f9398ce85ffda
MikKarter <66348029+MikKarter@users.noreply.github.com>
2022-08-28 19:46:14 +0900

    Add files via upload

---
+++ 2.Task_8/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Task_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretPhrase = "SECRET PHRACE";
+            string passwordToSecretPhrase = "password";
+            string userInputPasswordToSecretPhrase;
+            int maxNumberOfAttempts = 3;
+                       
+            for (int i=0; i<maxNumberOfAttempts; i++)
+            {
+                Console.WriteLine("If you want see secret phrase please input the password\n You have a " + (maxNumberOfAttempts-i) + " attempts" );
+                userInputPasswordToSecretPhrase = Console.ReadLine();
+                
+                if (i==maxNumberOfAttempts-1)
+                {
+                    Console.WriteLine("Incorrect password. Last attempt");
+                }
+
+                else if (userInputPasswordToSecretPhrase == passwordToSecretPhrase)
+                {
+                    Console.WriteLine(secretPhrase);
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect password. Last attempt");
+                }
+            }
+        }
+    }
+}
