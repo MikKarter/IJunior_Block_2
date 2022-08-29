abb384eadc4fe6f3e89c22687d3a53bafba7b496
MikKarter <66348029+MikKarter@users.noreply.github.com>
2022-08-29 09:49:54 +0900

    Add files via upload

---
+++ 2.Task_11/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Task_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userSymbolString;
+            Console.WriteLine("Please input you symbol string. Use only '(' and ')' symbols");
+            userSymbolString = Console.ReadLine();
+          
+            int counterSymbolOne = 0;
+            int counterSymbolTwo = 0;
+
+            foreach (var i in userSymbolString)
+            {
+
+                if (i=='(')
+                {
+                    counterSymbolOne++;
+
+                }
+                else if (i==')')
+                {
+                    counterSymbolTwo++;
+                }                
+            }
+            
+            if (counterSymbolOne == counterSymbolTwo)
+            {
+                Console.WriteLine("String is correct");
+                Console.WriteLine("Counter of deep - " + counterSymbolTwo);
+            }
+
+            else
+            {
+                Console.WriteLine("String is uncorrect");
+            }
+        }
+    }
+}
